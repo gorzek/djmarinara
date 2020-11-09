275ec93c14e9412ca5e9bfa232ca1c20d70d5353
Maxwell Plotkin <maxwell.a.plotkin@jpl.nasa.gov>
2020-11-08 17:25:31 -0800

    Reformat code

--- src/djmarinara/main.py
+++ src/djmarinara/main.py
@@ -5,14 +5,16 @@
 import djmarinara
 
 # Set configuration here.
-dj = djmarinara.djmarinara(extensions=['zip','xm','it','s3m','mod','mp3','mp4','flac','m4a','aac','flv','3gp', 'ogg', 'ra', 'rm'],
-            temppath="/tmp/songs",
-            mediapath="/media",
-            playlisturl="[YOUR_PLAYLIST_URL_HERE]",
-            fonturl="[YOUR_FONT_URL_HERE]",
-            startupvideo="[YOUR_STARTUP_VIDEO_HERE]",
-            gastanklimit=3600.0,
-            targetspeed=2.0)
+dj = djmarinara.djmarinara(
+    extensions=['zip', 'xm', 'it', 's3m', 'mod', 'mp3', 'mp4', 'flac', 'm4a', 'aac', 'flv', '3gp', 'ogg', 'ra', 'rm'],
+    temppath="/tmp/songs",
+    mediapath="/media",
+    playlisturl="[YOUR_PLAYLIST_URL_HERE]",
+    fonturl="[YOUR_FONT_URL_HERE]",
+    startupvideo="[YOUR_STARTUP_VIDEO_HERE]",
+    gastanklimit=3600.0,
+    targetspeed=2.0
+)
 # Run!
 # Will run until terminated forcefully.
 dj.run()
